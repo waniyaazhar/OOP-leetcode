13c0a6f3d3d042c4d75500d296c8984da8b519b7
Waniya Azhar <waniyaazhar24@gmail.com>
2025-04-08 16:28:10 +0500

    Time: 0 ms (100.00%) | Memory: 11.6 MB (45.48%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -3,7 +3,6 @@ public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
         int low = 0, mid = 0, high = n-1;
-
         while (mid <= high){
             if (nums[mid] == 0){
                 swap(nums[low], nums[mid]);
@@ -15,5 +14,6 @@ public:
                 high--;
             }
         }
+
     }
 };
